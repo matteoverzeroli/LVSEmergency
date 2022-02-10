d1d8a953554ca4d956ce217c23063839a1f3a12b
Davide Salvetti <davide.salvetti97@live.com>
2022-02-11 00:59:21 +0100

    Modifiche ai diagrammi. Modifiche alla documentazione.

--- ClientApp/PacClientApp/src/mastercontroller.h
+++ ClientApp/PacClientApp/src/mastercontroller.h
@@ -11,13 +11,13 @@
 
 /*!
  * \brief The MasterController class
- * \details Classe principale per l'applicazione.
+ * \details Classe principale dell'applicazione.
+ * Espone altre classi al QmlEngine.
  */
 
 class MasterController : public QObject
 {
     Q_OBJECT
-    // rendo accessibili le classi al lato QML
     Q_PROPERTY(NavigationController *ui_navigationController READ getNavigationController CONSTANT)
     Q_PROPERTY(accountManagement::UserController *ui_userController READ getUserController CONSTANT)
     Q_PROPERTY(teamManagement::TeamController *ui_teamController READ getTeamController CONSTANT)
