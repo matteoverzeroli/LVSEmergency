9706cb098da27666dddd7aa760f44a9122665c0c
Lorenzo Leoni <l.leoni2@studenti.unibg.it>
2022-02-17 00:37:05 +0100

    Correzione query data analyzer, traduzione in inglese dei commenti e aggiornamento __debug

--- DataCollector e DataAnalyzer/Data Analyzer_Collecor_And_Test/dataAnalyzer.py
+++ DataCollector e DataAnalyzer/Data Analyzer_Collecor_And_Test/dataAnalyzer.py
@@ -7,7 +7,7 @@ import time as tm
 from datetime import datetime as dt
 from threading import Thread as th
 
-class BadWeatherAllertsCreator(th):
+class BadWeatherAlertsCreator(th):
     def __init__(self, station_code, areaID, conn2, cursor2, index, old_final_time_list, old_delta_list):
         th.__init__(self)
         self.station_code = station_code
@@ -20,28 +20,28 @@ class BadWeatherAllertsCreator(th):
         self.recent_time = dt.strptime("2021-04-06 15:00:00", '%Y-%m-%d %H:%M:%S')
     
     def run(self):
-        # accesso al DB per ottenere la data dell'ultima acquisizione 
+        # access to date and time of last acquisition 
         self.cursor2.execute("SELECT max(time) FROM aprsdata WHERE name = %(code)s;", {'code':self.station_code})
         temporary = self.cursor2.fetchall()
         self.recent_time = list(temporary[0])[0]
         
-        # interruzione del thread se dovesse essere già stata elaborata la serie storica 
-        # contenente l'ultima acquisizione nel DB
-        if self.recent_time == self.old_final_time_list[self.index]:
-            print("No new row")
-            self.conn2.close()
-            self.cursor2.close()
-            return
+        # interruption of current thread if should be already analyzed the time series 
+        # with the last  acquisition in DB
+        if self.old_final_time_list[self.index] is not None:
+            if self.recent_time == self.old_final_time_list[self.index]:
+                print("No new row")
+                self.conn2.close()
+                self.cursor2.close()
+                return
         
-        # analisi ed elaborazione delle serie storica relativa alla pressione atmosferica delle ultime 24 ore
+        # analysis and processing of atmospheric pressure time series regarding last 24 hours
         pressure_DS = pd.DataFrame(columns=['time','pressure'])
-        self.cursor2.execute("SELECT time, pressure FROM aprsdata WHERE name = %(code)s", {'code':self.station_code})
+        self.cursor2.execute("SELECT time, pressure FROM aprsdata WHERE\
+                             time >= date_sub(now(), INTERVAL 1 DAY) and name = %(code)s", {'code':self.station_code})
         info = self.cursor2.fetchall()
         for i in range(len(info)):
             pressure_DS.loc[i] = list(info[i])
-        min_24h = self.recent_time - datetime.timedelta(hours=24)
         min_3h = self.recent_time - datetime.timedelta(hours=3)
-        pressure_DS = pressure_DS[pressure_DS.time > min_24h]
         y_24h = list(pressure_DS['pressure'])
         p_mean = sum(y_24h)/len(y_24h)
         for i in list(pressure_DS.index):
@@ -52,24 +52,24 @@ class BadWeatherAllertsCreator(th):
         p_up = (y_3h[-2] + y_3h[-3])/2
         delta = p_up - p_low
         
-        # generazione delle allerte relative al maltempo
+        # bad weather alerts creation
         if(delta >= 0):
-            self.__allertsCreator("C1", delta)
+            self.__alertsCreator("C1", delta)
         elif delta > self.old_delta_list[self.index]:
-            self.__allertsCreator("C2", delta)
+            self.__alertsCreator("C2", delta)
         elif delta <= -5:
-            self.__allertsCreator("C3", delta)
+            self.__alertsCreator("C3", delta)
         else:
-            self.__allertsCreator("C4", delta)
-            
-        # aggiornamenti degli ultimi delta e tf, commit, chiusura della connessione e terminazione del thread
+            self.__alertsCreator("C4", delta)
+        
+        # updating last delta and tf, commit, closing DB connection and thread ending 
         self.old_final_time_list[self.index] = self.recent_time
         self.old_delta_list[self.index] = delta
         self.conn2.commit()
         self.conn2.close()
         self.cursor2.close()
         
-    def __allertsCreator(self, case, delta):
+    def __alertsCreator(self, case, delta):
         if case == "C1":
             coulor = "NONE"
             description = "Pressione in aumento, delta = " + str(round(delta,2))
@@ -86,7 +86,7 @@ class BadWeatherAllertsCreator(th):
                               (dt.now().strftime('%Y-%m-%d %H:%M:%S'), "BW", coulor, int(self.areaID), description))
         
 
-class FogFrostAllertsCreator (th):
+class FogFrostAlertsCreator (th):
     def __init__(self, station_code, areaID, conn1, cursor1, index, old_final_time_list):
         th.__init__(self)
         self.station_code = station_code
@@ -94,31 +94,32 @@ class FogFrostAllertsCreator (th):
         self.conn1 = conn1
         self.cursor1 = cursor1
         self.index = index
-        self.old_final_time = old_final_time_list[self.index]
+        self.old_final_time_list = old_final_time_list
         self.recent_time = dt.strptime("2021-04-06 15:00:00", '%Y-%m-%d %H:%M:%S')
     
     def run(self):
-        # accesso al DB per ottenere la data dell'ultima acquisizione
+        # access to date and time of last acquisition
         self.cursor1.execute("SELECT max(time) FROM aprsdata WHERE name = %(code)s;", {'code':self.station_code})
         temporary = self.cursor1.fetchall()
         self.recent_time = list(temporary[0])[0]
         
-        # interruzione del thread se dovesse essere già stata elaborata la serie storica 
-        # contenente l'ultima acquisizione nel DB
-        if self.recent_time == self.old_final_time_list[self.index]:
-            print("No new row")
-            self.conn1.close()
-            self.cursor1.close()
-            return
+        # interruption of current thread if should be already analyzed the time series 
+        # with the last  acquisition in DB
+        if self.old_final_time_list[self.index] is not None:
+            if self.recent_time == self.old_final_time_list[self.index]:
+                print("No new row")
+                self.conn1.close()
+                self.cursor1.close()
+                return
         
-        # creazione dei dataframe per i parametri meteorologici e avvio dei predittori
+        # creation of tables for meteorological parameters and forcasters starting
         summaryT = pd.DataFrame(index=["tf","t1","t2","t3"], columns=["I_low", "value", "I_up"])
         summaryUR = pd.DataFrame(index=["tf","t1","t2","t3"], columns=["I_low", "value", "I_up"])
         summaryTd = pd.DataFrame(index=["tf","t1","t2","t3"], columns=["I_low", "value", "I_up"])
         self.__tempURForcaster("temperature", summaryT)
         self.__tempURForcaster("humidity", summaryUR)
         
-        # determinazione dei valori della temperatura di rugiada e rispettivi intervalli di accettazione
+        # determination of dew point's values and rispective acceptance ranges
         for j in list(summaryTd.columns):
             for i in list(summaryTd.index):
                 summaryTd.at[i,j] = self.__computeTd(summaryT.at[i,j], summaryUR.at[i,j])
@@ -134,60 +135,62 @@ class FogFrostAllertsCreator (th):
         print(summaryUR)
         print(summaryTd) 
         
-        # generazione delle allerte relative alle formazioni di nebbia e brina
+        # fog and frost alerts creation
         if summaryT.at['tf','value'] >= summaryTd.at['tf','I_low'] and summaryT.at['tf','value'] <= summaryTd.at['tf','I_up']:
            if summaryT.at['tf','value'] > 0:
-               self.__allertsCreator("tf", "FOG")
+               self.__alertsCreator("tf", "FOG")
                self.__debug("tf", "FOG", summaryT, summaryTd)
            else:
-               self.__allertsCreator("tf", "FROST")
+               self.__alertsCreator("tf", "FROST")
                self.__debug("tf", "FROST", summaryT, summaryTd)
         elif summaryT.at['t1','value'] >= summaryTd.at['t1','I_low'] and summaryT.at['t1','value'] <= summaryTd.at['t1','I_up']:
            if summaryT.at['t1','value'] > 0:
-               self.__allertsCreator("t1", "FOG")
+               self.__alertsCreator("t1", "FOG")
                self.__debug("t1", "FOG", summaryT, summaryTd)
            else:
-               self.__allertsCreator("t1", "FROST")
+               self.__alertsCreator("t1", "FROST")
                self.__debug("t1", "FROST", summaryT, summaryTd)
         elif summaryT.at['t2','value'] >= summaryTd.at['t2','I_low'] and summaryT.at['t2','value'] <= summaryTd.at['t2','I_up']:
            if summaryT.at['t2','value'] > 0:
-               self.__allertsCreator("t2", "FOG")
+               self.__alertsCreator("t2", "FOG")
                self.__debug("t2", "FOG", summaryT, summaryTd)
            else:
-               self.__allertsCreator("t2", "FROST")
+               self.__alertsCreator("t2", "FROST")
                self.__debug("t2", "FROST", summaryT, summaryTd)
         elif summaryT.at['t3','value'] >= summaryTd.at['t3','I_low'] and summaryT.at['t3','value'] <= summaryTd.at['t3','I_up']:
            if summaryT.at['t3','value'] > 0:
-               self.__allertsCreator("t3", "FOG")
+               self.__alertsCreator("t3", "FOG")
                self.__debug("t3", "FOG", summaryT, summaryTd)
            else:
-               self.__allertsCreator("t3", "FROST")
+               self.__alertsCreator("t3", "FROST")
                self.__debug("t3", "FROST", summaryT, summaryTd)
         else:
-            self.__allertsCreator()
+            self.__alertsCreator()
+            self.__debug("tf", "NONE", summaryT, summaryTd)
         
-        # aggiornamenti dell'ultimo tf, commit, chiusura della connessione e terminazione del thread
+        # updating last tf, commit, closing DB connection and thread ending
         self.old_final_time_list[self.index] = self.recent_time
         self.conn1.commit()
         self.conn1.close()
         self.cursor1.close()
         
     def __tempURForcaster(self, parameter, dataToUpdate):
-        # analisi ed elaborazione delle serie storica del parametro temperatura o umidità relativa 
-        # delle ultime 3 ore a scopo predittivo
+        # analysis and processing of temperature or relative umidity's time series regarding
+        # last 24 hours for predictive purpose
         if parameter == "temperature":
-            self.cursor1.execute("SELECT time, temperature FROM aprsdata WHERE name = \
-                       %(station)s;", {'station':self.station_code})
+            self.cursor1.execute("SELECT time, temperature FROM aprsdata WHERE\
+                                 time >= date_sub(now(), INTERVAL 1 DAY)\
+                                     and name = %(station)s;", {'station':self.station_code})
         else:
-            self.cursor1.execute("SELECT time, humidity FROM aprsdata WHERE \
-                           name = %(station)s;", {'station':self.station_code})
+            self.cursor1.execute("SELECT time, humidity FROM aprsdata WHERE\
+                                 time >= date_sub(now(), INTERVAL 1 DAY)\
+                                     and name = %(station)s;", {'station':self.station_code})
         info = self.cursor1.fetchall()
         parameter_DS = pd.DataFrame(columns=["time", parameter])
         for i in range(len(info)):
             parameter_DS.loc[i] = list(info[i])
-        min_time = self.recent_time - datetime.timedelta(hours=24)
-        parameter_DS = parameter_DS[parameter_DS.time > min_time]
         y = parameter_DS[parameter]
+        print(y)
         model = pm.auto_arima(y, start_p=1, start_q=1,
                               test='adf',
                               max_p=3, max_q=3,
@@ -202,7 +205,7 @@ class FogFrostAllertsCreator (th):
                               stepwise=True)
         fc, confint = model.predict(n_periods=3, return_conf_int=True)
         
-        # aggiornamento dei dataframe
+        # updating tables
         dataToUpdate.at['tf', 'value'] = parameter_DS.iat[len(parameter_DS)-1,1]
         dataToUpdate.at['t1', 'value'] = fc[0]
         dataToUpdate.at['t2', 'value'] = fc[1]
@@ -227,7 +230,7 @@ class FogFrostAllertsCreator (th):
         b = 237.7
         return ((a*T)/(b+T)) + np.log(UR/100)
     
-    def __allertsCreator(self, t=None, typology=None):
+    def __alertsCreator(self, t=None, typology=None):
         if t is not None:
             if t == "tf":
                 coulor = "RED"
@@ -255,38 +258,57 @@ class FogFrostAllertsCreator (th):
                               (dt.now().strftime('%Y-%m-%d %H:%M:%S'), typology, "NONE", int(self.areaID), description))
                 
     def __debug(self, t, typology, summaryT, summaryTd):
-        delta = None
-        if t == "tf":
-            delta = "now"
-        elif t == "t1":
-            delta = "10 minutes"
-        elif t == "t2":
-            delta = "20 minutes"
+        if typology != "NONE":
+            delta = None
+            if t == "tf":
+                delta = "now"
+            elif t == "t1":
+                delta = "10 minutes"
+            elif t == "t2":
+                delta = "20 minutes"
+            else:
+                delta = "30 minutes"
+            print(" ")
+            print("***")
+            print("CAUTION! " + str.lower(typology) + " allert")
+            print("-ID area: " + str(self.areaID))
+            print("-Recent time: " + dt.now().strftime('%Y-%m-%d %H:%M:%S'))
+            print("-Final time: " + self.recent_time.strftime('%Y-%m-%d %H:%M:%S'))
+            print("-Delta: " + delta)
+            print("-Air temperature: " + str(summaryT.at[t, "value"]) + "°C")
+            print("-Dew point interval: [" + str(round(summaryTd.at[t, "I_low"], 2)) + "°C; " + str(round(summaryTd.at[t, "I_up"], 2)) + "°C]")
+            print("***")
+            print(" ")
         else:
-            delta = "30 minutes"
-        print(" ")
-        print("***")
-        print("CAUTION! " + str.lower(typology) + " allert")
-        print("-ID area: " + str(self.areaID))
-        print("-Recent time: " + dt.now().strftime('%Y-%m-%d %H:%M:%S'))
-        print("-Final time: " + self.recent_time.strftime('%Y-%m-%d %H:%M:%S'))
-        print("-Delta: " + delta)
-        print("-Interval: [" + str(summaryTd.at[t, "I_low"]) + "; " + str(summaryT.at[t, "value"]) + "; " + str(summaryTd.at[t, "I_up"]) + "]")
-        print("***")
-        print(" ")
+            print(" ")
+            print("***")
+            print("No fog or frost risk")
+            print("-ID area: " + str(self.areaID))
+            print("-Recent time: " + dt.now().strftime('%Y-%m-%d %H:%M:%S'))
+            print("-Final time: " + self.recent_time.strftime('%Y-%m-%d %H:%M:%S'))
+            print("-Delta: now")
+            print("-Air temperature: " + str(summaryT.at[t, "value"]) + "°C")
+            print("-Dew point interval: [" + str(round(summaryTd.at[t, "I_low"], 2)) + "°C; " + str(round(summaryTd.at[t, "I_up"], 2)) + "°C]")
+            print("***")
+            print(" ")
         
 def algorithm(database, isTest):
-     # definizione dei parametri per aprire la connessione con il DB
+    
+    # parameters' definition to establish DB connection
     config = {
       'host':'progettopacdb.mysql.database.azure.com',
       'user':'dsalvetti',
       'password':'Progettopac2021!',
       'database':database
     }
-
-    # inizializzazione delle variabili di supporto all'esecuzione dei thread
+    
+    # initialization of supporting variables for thread's execution 
+    n_areas = 100
+    old_final_time_list = [None]*n_areas
+    old_delta_list = [0]*n_areas
+    
     while True:
-        # apertura della connessione con il DB e definizione del sistema di riconnessione automatico
+        # opening DB connection and automatic reconnection system's definition 
         connected = False
         while connected == False:
             try:
@@ -301,48 +323,42 @@ def algorithm(database, isTest):
                 print(" ")
                 print(e)
                 tm.sleep(15)
-
-        # accesso ai dati relativi alle zone
+                
+        # access to zones data 
         cursor.execute("SELECT * FROM area;")
         rows = cursor.fetchall()
-
-        n_areas = len(rows) # maximum number of areas
-        old_final_time_list = [None]*n_areas
-        old_delta_list = [0]*n_areas
-
         area_DS = pd.DataFrame(columns=["idArea","areaName","lat","lng","nameAprStation","istatCode"])
         num_areas = len(rows)
         for i in range(num_areas):
             area_DS.loc[i] = list(rows[i])
-   
-        # creazione dei thread per la generazione delle allerte nebbia, brina e maltempo, uno per ogni zona
-        fogFrostAllertsCreators = []
-        badWeatherAllertsCreators = []
-
+            
+        # creation of threads for fog, frost and bad weather alerts creation, one for every zone
+        fogFrostAlertsCreators = []
+        badWeatherAlertsCreators = []
         for i in range(num_areas):
             if area_DS.at[i, "nameAprStation"] is not None:
                 conn1 = mysql.connector.connect(**config)
                 cursor1 = conn1.cursor()
                 conn2 = mysql.connector.connect(**config)
                 cursor2 = conn2.cursor()
-                t = FogFrostAllertsCreator(area_DS.at[i, "nameAprStation"], area_DS.at[i, "idArea"], conn1, cursor1, i, old_final_time_list)
-                w = BadWeatherAllertsCreator(area_DS.at[i, "nameAprStation"], area_DS.at[i, "idArea"], conn2, cursor2, i, old_final_time_list, old_delta_list)
-                fogFrostAllertsCreators.append(t)
-                badWeatherAllertsCreators.append(w)
-    
-        # avvio dei thread         
-        for i in fogFrostAllertsCreators:
+                t = FogFrostAlertsCreator(area_DS.at[i, "nameAprStation"], area_DS.at[i, "idArea"], conn1, cursor1, i, old_final_time_list)
+                w = BadWeatherAlertsCreator(area_DS.at[i, "nameAprStation"], area_DS.at[i, "idArea"], conn2, cursor2, i, old_final_time_list, old_delta_list)
+                fogFrostAlertsCreators.append(t)
+                badWeatherAlertsCreators.append(w)
+        
+        # starting threads         
+        for i in fogFrostAlertsCreators:
             i.start()
-        for i in badWeatherAllertsCreators:
+        for i in badWeatherAlertsCreators:
             i.start()
-    
-        # join dei thread precedentemete avviati
-        for i in fogFrostAllertsCreators:
+            
+        # join threads
+        for i in fogFrostAlertsCreators:
             i.join()
-        for i in badWeatherAllertsCreators:
+        for i in badWeatherAlertsCreators:
             i.join()
-    
-        # chiusura della connessione con il DB
+            
+        # closing DB connection
         cursor.close()
         conn.close()
         print("Done.")
@@ -352,7 +368,7 @@ def algorithm(database, isTest):
         if isTest:
             break
 
-        tm.sleep(60*5)
+        tm.sleep(5)
 
 if __name__ == '__main__':
     algorithm("test", False)
